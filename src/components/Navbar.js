d0e825dc7cc90bb220e3d6411ee1ba7850e512c6
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-09-04 09:01:42 +0330

    coding about navbars components (js and css)

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { FaBars,FaTimes } from 'react-icons/fa'
+import './Navbar.css'
+import { useState } from 'react'
+
+function Navbar() {
+ 
+  const [click,setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
+  return (
+    <div className='header'>
+      <div className='container'>
+        <h1>پور<span className='primary'>یا</span></h1>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li>
+            <a href="">خانه</a>
+          </li>
+          <li>
+            <a href="">دسترسی ها</a>
+          </li>
+          <li>
+            <a href="">درامد</a>
+          </li>
+          <li>
+            <a href="">ارتباط با ما</a>
+          </li>
+        </ul>
+        <div className='btm-group'>
+          <button className='btn'>اتصال به کیف پول</button>
+        </div>
+
+        <div className='hamburger' onClick={handleClick}>
+          {click ? <FaTimes size={20} style={{color: '#333'}} /> : (
+            <FaBars size={20} style={{color: '#333'}} />
+          )}
+          
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
